d726303d530c6c8c8b2363f30e420db14af67c44
Manaswini123456 <janu2001dec@gmail.com>
2023-12-07 23:08:16 +0530

    ChatApp fine

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,20 +1,32 @@
-const generateMessage = (username,text) =>{
-     return{
-         username,
-         text,
-         createdAt : new Date().getTime()
-     }
-}
+/**
+ * Generates a message object with username, text, and creation timestamp.
+ * @param {string} username - The username associated with the message.
+ * @param {string} text - The content of the message.
+ * @returns {Object} - The generated message object.
+ */
+const generateMessage = (username, text) => {
+    return {
+        username,
+        text,
+        createdAt: new Date().getTime(),
+    };
+};
 
-const generateLocationMessage = (username,url) =>{
+/**
+ * Generates a location message object with username, URL, and creation timestamp.
+ * @param {string} username - The username associated with the location message.
+ * @param {string} url - The URL representing the location.
+ * @returns {Object} - The generated location message object.
+ */
+const generateLocationMessage = (username, url) => {
     return {
         username,
         url,
-        createdAt : new Date().getTime()
-    }
-}
+        createdAt: new Date().getTime(),
+    };
+};
 
 module.exports = {
     generateMessage,
-    generateLocationMessage
-}
+    generateLocationMessage,
+};
